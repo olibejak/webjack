d2641c3ade7d79d3ac877d63ae161a9dad5d89ec
Jakub Oliberius <olibejak@fel.cvut.cz>
2024-06-02 23:07:40 +0200

    Welcome board information

--- src/Components/WelcomeBoard/WelcomeBoard.tsx
+++ src/Components/WelcomeBoard/WelcomeBoard.tsx
@@ -23,18 +23,22 @@ function WelcomeBoard({onStartGame}: {onStartGame: any}) {
             <div className={"info"}>
                 <h2>About:</h2>
                 <article>
+                    <p>This site is a university project for subject KAJ at CTU FEE made by Jakub Oliberius.</p>
+                    <h3>Game information:</h3>
+                    <ul>
+                        <li>The blue highlighted player has turn</li>
+                        <li>The red highlighted player is standing</li>
+                        <li>Cards from deck can be dragged and dropped to the active player area for triggering hit</li>
+                        <li>Player can change bet pre game</li>
+                        <li>Player data are stored in local storage</li>
+                        <li>Players with no chips will be removed</li>
+                    </ul>
+                    <h3>Links:</h3>
                     <p>
                         About Blackjack: <a href={"https://en.wikipedia.org/wiki/Blackjack"}
                                             target="_blank"
                                             rel="noreferrer noopener">
                         Wikipedia</a>
-                    </p>
-                    <p>
-                        How to play:
-                        <ul>
-                            <li>The blue highlighted player has turn</li>
-                            <li></li>
-                        </ul>
                     </p>
                     <p>
                         API used: <a href={"https://www.deckofcardsapi.com"}
@@ -67,7 +71,8 @@ function WelcomeBoard({onStartGame}: {onStartGame: any}) {
             </div>
             <button
                 id={"start-button"}
-                onClick={handleStartGame}>Start Game</button>
+                onClick={handleStartGame}>Start Game
+            </button>
         </div>
     )
 }
